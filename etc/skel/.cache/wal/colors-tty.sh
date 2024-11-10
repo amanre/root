ed89ab5ed27b0159a5982efe3b07b41790f89976
amanre <amanre556@gmail.com>
2024-11-10 17:35:18 +0100

    update

---
+++ etc/skel/.cache/wal/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P0170d0e
+                 \e]P1A15551
+                 \e]P2CD695E
+                 \e]P3BE847B
+                 \e]P46C7C83
+                 \e]P577888E
+                 \e]P677BFD2
+                 \e]P7c3c4c5
+                 \e]P8888989
+                 \e]P9A15551
+                 \e]PACD695E
+                 \e]PBBE847B
+                 \e]PC6C7C83
+                 \e]PD77888E
+                 \e]PE77BFD2
+                 \e]PFc3c4c5
+                 \ec'
