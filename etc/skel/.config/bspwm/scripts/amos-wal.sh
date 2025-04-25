46b1cf32e2e92ecf735087c640bd162e7228508e
amanre <amanre556@gmail.com>
2025-04-25 12:44:58 +0200

    update

--- etc/skel/.config/bspwm/scripts/amos-wal.sh
+++ etc/skel/.config/bspwm/scripts/amos-wal.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ### Check if python-pywal is installed
 if [ -x "$(command -v wal)" ]; then
-    my_array=(/usr/share/backgrounds/wallpaper/sunset-10.jpg)
+    my_array=(/usr/share/backgrounds/walls/sunset-10.jpg)
         ####my_array=(/usr/share/backgrounds/*)
      wal -i ${my_array[$(( $RANDOM % ${#my_array[@]}))]} -a 80
 else
   # Set background
   if [ -x "$(command -v feh)" ]; then
-    feh --randomize --bg-fill /usr/share/backgrounds/wallpaper/*
+    feh --randomize --bg-fill /usr/share/backgrounds/walls/*
     /wallpapers/*
       ####feh --randomize --bg-fill /usr/share/backgrounds/*
   fi
